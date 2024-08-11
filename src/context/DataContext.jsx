72424fa3f901ddf17e4546f76dfbff318ad61269
GabrielFerrin <gabrielferrin@byui.edu>
2024-08-11 01:38:43 -0500

    update

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -25,7 +25,9 @@ export const DataProvider = ({ children }) => {
   })
 
   useEffect(() => {
-    verify.mutate()
+    const tempToken = localStorage.getItem('token')
+    if (tempToken) verify.mutate()
+    else setToken('')
     // eslint-disable-next-line
   }, [])
 
